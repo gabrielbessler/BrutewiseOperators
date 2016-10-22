3ce7d42902f86f3b7f0f56dead66bbaf2211ee47
Gabriel Bessler <gabrielbessler@gmail.com>
2016-10-22 01:20:23 -0700

    Organizing
    Not final. This file will not run.

--- RandomOperator.py
+++ RandomOperator.py
@@ -1,45 +1,97 @@
-# Random operator generator
-import sys
+# Random Operator Generator
+#
+# Description: 
+# This program uses a brute force approach to finding a list of expressions 
+# involving bitwise operations and two numbers that are equivalent to normal
+# base 10 operations between those same two numbers.
+#
+# Specifically:
+# 
+# Take [x] as a bitwise operator (e.g. <<, >>, &, |)
+# Take [y] as base a "base 10" operator (e.g. +, -, *, /)
+# Take A, B to be two positive integers s.t. A > B
+#
+# Type One: 
+# (A [x] B) [y] (A [x] B) = A [y] B
+#
+
 from random import *
-L = ['<<', '>>', '&', '|' ]
-P = ['+', '-', '*', '/', '%', '**']
-#Z = ['<<', '>>', '&', '|', '+', '-', '*', '/', '%', '**']
+
+#List of basic bitwise operations
+B_operators = ['<<', '>>', '&', '|' ]
+#List of basic base 10 operations
+T_operators = ['+', '-', '*', '/', '%']
+#Previous list including x^n
+T2_operators = ['+', '-', '*', '/', '%', '**']
+#List containing both bitwise and base 10 operations
+All_operators = ['<<', '>>', '&', '|', '+', '-', '*', '/', '%', '**']
+#Method of finding expressions
+#0 - Exhaustive Search (Interate through all posibilities for given sets of T_ops and B_ops)
+#1 - Sampling (Pick random posibilities from within T_ops and B_ops)
+COMP_METHOD = 1
+#Number of iterations (Program will test REPEAT_NUM ^2 different expressions)
+REPEAT_NUM = 10
 
 def Main():
-    '''DOCSTRING'''
+    '''Manages the computation and output of results.'''
     #Stores all of the results
+    #Valid expressions of Type One as stored as [B1, T1, B2, T2]
     Results = []
+    
+    #TODO: Remove? Or add as internal testing feature?
     NumberPairs = []    
+    
     #Number of iterations (Caution: runs NUM^2 times)
     REPEAT_NUM = 10
-    
-    for i in list(range(REPEAT_NUM)):
-        for i2 in list(range(REPEAT_NUM)):
-            answer = r(str(i), str(i2))
-            if answer != None:
-                Results += [answer[0:4]]
-                NumberPairs += [answer[4:]] 
-    
-    #lenResults is used for seeing number of duplicates
-    #lenResultsBeforeFiltering = len(Results)
+        
+    if COMP_METHOD == 0:
+        #Exhaustive Search
+        return 0
+    elif COMP_METHOD == 1:
+        #Sampling
+        for n1 in list(range(REPEAT_NUM)):
+            for n2 in list(range(REPEAT_NUM)):
+                answer = r(str(n1), str(n2))
+                if answer != None:
+                    Results += [answer[0:4]]
+                    NumberPairs += [answer[4:]] 
+                    
+    originalLenResults = len(Results)
+    #Remove duplicates in Results
     Results = filter(Results)
-    #lenResultsAfterFiltering = len(Results)
+    #Stores the percentage of duplicates found
+    filteringEfficiency = (originalLenResults - len(Results))/len(results)
     
+    #Checks if results are actually valid
     for i in Results:
-        #print(i)
-        #print(calculate(i))
         if calculate(i) == True:
+            #TODO: Easier way to do this?
             correct = True
-            j = 0
-            while j > 0: 
+            timesToTest = 5
+            while timesToTest > 0: 
                 if calculate(i) == False:
                     correct = False
+                    break
                 j -= 1
             if correct == True:
                 print(i)
-
+                
+def r(num1, num2):
+    '''Input: Two integers: num1, num2
+       Output: '''
+    try:
+        a = choice(range(len(B_operators)))
+        b = choice(range(len(B_operators)))
+        c = choice(range(len(T_operators)))
+        d = choice(range(len(T_operators)))
+        if eval(str(eval(num1+L[a]+num2))+P[b]+str(eval(num1+L[c]+num2)))== eval(num1+P[d]+num2):
+            return([L[a],P[b],L[c],P[d],num1,num2])
+    except:
+        #Often a divide by zero error
+        pass
+    
 def filter(L):
-    '''DOCSTRING'''
+    '''Removes duplicate items from a list L'''
     if len(L) <= 1:
         return L
     if L[0] in L[1:]: 
@@ -53,22 +105,13 @@ def calculate(L):
     try:
         if eval( str(eval('a'+L[0]+'b')) + L[1] + str(eval('a'+L[2]+'b')) ) == eval('a'+L[3]+'b'):
             return True
-        else:
-            return False
+        return False
     except: 
         pass
 
-def r(a, b):
-    '''DOCSTRING'''
-    try:
-        c = choice(range(len(L)))
-        e = choice(range(len(L)))
-        d = choice(range(len(P)))
-        f = choice(range(len(P)))
-        if eval(str(eval(a+L[c]+b))+P[d]+str(eval(a+L[e]+b)))== eval(a+P[f]+b):
-            return([L[c],P[d],L[e],P[f],a,b])
-    except:
-        pass
-
+#Initializer  
+if __name__ == "__main__":
+    main()
+    
 # if eval(str(eval(a+Z[c]+b))+Z[d]+str(eval(a+Z[e]+b)))== eval(a+Z[f]+b):
 # return([Z[c],Z[d],Z[e],Z[f],a,b])
